f2ab3e927a979ad9408b32a6c476b815157d52aa
VictorTavares-Dev <victor.tavares314@outlook.com>
2024-10-06 21:37:28 -0300

    add: initial infrastructure configuration files

---
+++ infra/modules/s3/main.tf
@@ -0,0 +1,12 @@
+# Create roles
+
+# Config Lake Formation by removing IAMAllowedPrincipals and setting role
+# control-lf-admin-x-account-role as data lake admin
+
+# Create SQS queues
+
+# Create S3 bucket
+
+# Create Lambdas
+
+# Create DDB tables
